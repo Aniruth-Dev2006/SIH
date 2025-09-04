0fad14edc0057f677a49cf374b0dad36841931ee
Aniruth-Dev2006 <aniruthrajagopal2006@gmail.com>
2025-09-04 22:09:47 +0530

    search users completed

--- app.js
+++ app.js
@@ -6,20 +6,29 @@ const  bodyParser= require("body-parser");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("Public"));
 app.set("view engine", "ejs");
+var ad = "panda";
 mongoose.connect("mongodb://localhost:27017/communityDB");
 const adminschema = new mongoose.Schema({
     email:String,
-    password:Number
+    password:String
 });
 const Admin = mongoose.model("admin",adminschema);
 const studentschema = new mongoose.Schema({
+    name:String,
     email:String,
-    password:Number
+    password:String,
+    batch:String,
+    role:String,
+    rno:String
 });
 const Student = mongoose.model("student",studentschema);
 const aluminschema = new mongoose.Schema({
+    name:String,
     email:String,
-    password:Number
+    password:String,
+    batch:String,
+    role:String,
+    rno:String
 });
 const Alumini = mongoose.model("alumini",aluminschema);
 app.get("/",function(req,res){
@@ -31,6 +40,7 @@ app.post("/", function (req, res) {
   Admin.findOne({ email, password })
     .then(admin => {
       if (admin) {
+        ad = req.body.email;
         res.render("admin", { admin: req.body.email });
         return null; 
       }
@@ -57,7 +67,49 @@ app.post("/", function (req, res) {
       res.status(500).send("Server error");
     });
 });
+app.get("/user_management",function(req,res){
+  res.render("user_management",{admin:ad});
+});
 
+app.get("/all_users",function(req,res){
+  Alumini.find()
+    .then(alumni=>{
+      Student.find()
+        .then(stud=>{
+          res.render("all_users",{users:alumni,students:stud});
+        })
+        .catch(err=>{
+          console.log("error "+err);
+        });
+    })
+    .catch(err=>{
+      console.log("error "+err);
+    })
+});
+
+app.post("/all-users",function(req,res){
+  if(req.body.role==="Student"){
+    Student.find({name:req.body.name,batch:req.body.batch})
+      .then(result=>{
+        res.render("all_users",{users:result,students:[]});
+      })
+      .catch(err=>{
+        console.log("error "+err);
+      });
+  }else{
+    Alumini.find({name:req.body.name,batch:req.body.batch})
+      .then(result=>{
+        res.render("all_users",{users:result,students:[]});
+      })
+      .catch(err=>{
+        console.log("error "+err);
+      });
+  }
+});
+
+app.get("/admin",function(req,res){
+  res.render("admin",{admin:ad});
+});
 
 app.listen(3000,function(req,res){
     console.log("server is running\n");
